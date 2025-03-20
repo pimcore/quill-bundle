6719031dd2b981843da7dced12b74eb32d3ccd63
markus-moser <markus.moser@pimcore.com>
2025-03-20 12:57:09 +0100

    Try to fix pipeline

--- assets/studio/js/src/quill-editor/quill-editor.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { type WysiwygProps } from '@pimcore/studio-ui-bundle/modules/wysiwyg'
-
-const QuillEditor: React.FC = (props: WysiwygProps) => {
-  return <div>Dummy Quill Editor Component</div>
-}
-
-export default QuillEditor

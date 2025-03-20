6bdd26b2cb33e49b2ed67916d91d33524dca5eb3
markus-moser <markus.moser@pimcore.com>
2025-03-20 12:36:21 +0100

    Fix

--- assets/studio/js/src/main.ts
+++ assets/studio/js/src/main.ts
@@ -1,6 +1,5 @@
 import { Pimcore } from '@pimcore/studio-ui-bundle'
-// import { serviceIds } from '@pimcore/studio-ui-bundle/app'
-import QuillEditor from './quill-editor/quill-editor'
+import { QuillEditorModule } from './modules/quill-editor'
 
 if (module.hot !== undefined) {
   module.hot.accept()
@@ -11,8 +10,7 @@ Pimcore.pluginSystem.registerPlugin({
 
   // Register and overwrite services here
   onInit: ({ container }): void => {
-    container.rebind('wysiwyg').toConstantValue(QuillEditor)
-    console.log('hello from quill.')
+
   },
 
   // register modules here

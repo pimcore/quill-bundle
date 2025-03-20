8cf4d1deab52fff455f85770d0cbd112af8eae32
markus-moser <markus.moser@pimcore.com>
2025-03-20 13:15:11 +0100

    Fix pipeline

--- assets/studio/js/src/main.ts
+++ assets/studio/js/src/main.ts
@@ -16,6 +16,6 @@ Pimcore.pluginSystem.registerPlugin({
   // register modules here
   onStartup: ({ moduleSystem }): void => {
     moduleSystem.registerModule(QuillEditorModule)
-    alert('Hello from quill...')
+    alert('Hello from quill..')
   }
 })

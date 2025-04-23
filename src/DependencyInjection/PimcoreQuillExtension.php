f1403ad5367e4da9235d377a10639079c5f65796
markus-moser <markus.moser@pimcore.com>
2025-04-23 16:32:26 +0200

    Improve studio optionality

--- src/DependencyInjection/PimcoreQuillExtension.php
+++ src/DependencyInjection/PimcoreQuillExtension.php
@@ -17,12 +17,16 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-final class PimcoreQuillExtension extends Extension
+final class PimcoreQuillExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+    }
 
+    public function prepend(ContainerBuilder $container): void
+    {
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../../config')

0fdf612f99ec9e071d62db6f5d040c0494ada826
markus-moser <4639428+markus-moser@users.noreply.github.com>
2025-04-23 14:32:48 +0000

    Apply php-cs-fixer changes

--- src/DependencyInjection/PimcoreQuillExtension.php
+++ src/DependencyInjection/PimcoreQuillExtension.php
@@ -16,8 +16,8 @@ namespace Pimcore\Bundle\QuillBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class PimcoreQuillExtension extends Extension implements PrependExtensionInterface
 {

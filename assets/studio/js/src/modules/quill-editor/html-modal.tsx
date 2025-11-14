521239ae0f722e92326688e4e2bb2c34484dbe80
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-11-14 12:27:16 +0100

    Add 'header' plugin to ESLint configuration

--- assets/studio/js/src/modules/quill-editor/html-modal.tsx
+++ assets/studio/js/src/modules/quill-editor/html-modal.tsx
@@ -1,3 +1,13 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
 import { Button, Modal, ModalFooter, TextArea } from '@pimcore/studio-ui-bundle/components'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from '@pimcore/studio-ui-bundle/app'

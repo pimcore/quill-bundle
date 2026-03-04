72fe7fb7ee9931036c19fbca75cc8d1d5d9b7a76
jcPimcore <jochen.califice@pimcore.com>
2026-03-04 09:16:06 +0100

    Improve: Removing ExtJS / Classis Admin UI related implementations

--- src/PimcoreQuillBundle.php
+++ src/PimcoreQuillBundle.php
@@ -15,14 +15,11 @@ namespace Pimcore\Bundle\QuillBundle;
 
 use Pimcore\Bundle\QuillBundle\DependencyInjection\PimcoreQuillExtension;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
-use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
-use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
 use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
-class PimcoreQuillBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
+class PimcoreQuillBundle extends AbstractPimcoreBundle
 {
-    use BundleAdminClassicTrait;
     use PackageVersionTrait;
 
     public function getContainerExtension(): ExtensionInterface
@@ -33,34 +30,5 @@ class PimcoreQuillBundle extends AbstractPimcoreBundle implements PimcoreBundleA
     public function getPath(): string
     {
         return dirname(__DIR__);
-    }
-
-    public function getJsPaths(): array
-    {
-        return [
-            '/bundles/pimcorequill/quill/quill.js',
-            '/bundles/pimcorequill/quill-table-better/quill-table-better.js',
-            '/bundles/pimcorequill/js/editor.js',
-        ];
-    }
-
-    public function getCssPaths(): array
-    {
-        return [
-            '/bundles/pimcorequill/css/editor.css',
-            '/bundles/pimcorequill/quill/quill.snow.css',
-            '/bundles/pimcorequill/quill/quill.bubble.css',
-            '/bundles/pimcorequill/quill-table-better/quill-table-better.css',
-        ];
-    }
-
-    public function getEditmodeCssPaths(): array
-    {
-        return $this->getCssPaths();
-    }
-
-    public function getEditmodeJsPaths(): array
-    {
-        return $this->getJsPaths();
     }
 }

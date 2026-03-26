e17d1832d6894dda7cbf89d131eb719b1ea0f76c
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-03-26 15:32:23 +0100

    fix: migrate ESLint to v9 flat config and remove unused devDependencies (#71)
    * update packages
    
    * Automatic frontend build
    
    * Automatic frontend build

---
+++ public/studio/build/be1b45bf-991e-43db-917d-508e7263fada/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_quill_bundle = "/bundles/pimcorequill/studio/build/be1b45bf-991e-43db-917d-508e7263fada/static/js/remoteEntry.js"
+
+      
+    

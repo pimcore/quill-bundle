f9fc50ea1f5717b1c5af7f1e992cca2729bb3123
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-03-26 14:03:19 +0000

    Automatic frontend build

---
+++ public/studio/build/383cfba6-1613-4cf2-8e87-c58e569a8191/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_quill_bundle = "/bundles/pimcorequill/studio/build/383cfba6-1613-4cf2-8e87-c58e569a8191/static/js/remoteEntry.js"
+
+      
+    
